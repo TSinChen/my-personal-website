b6a07d2ff9939198f15b1055383a9d4fe414bb7a
TSin Chen <sinin60076@gmail.com>
2021-12-18 21:25:42 +0800

    handle nav items clicked

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,21 @@
 import styles from './style.module.scss'
-
 import { NAV_ITEMS } from '../../constants/navItems'
 
 const Header = () => {
+  const handleJumpToAnchor = (id: keyof typeof NAV_ITEMS) => {
+    const headerHeight = document.getElementById('header')?.offsetHeight || 0
+    const position = document.getElementById(id)?.offsetTop || headerHeight
+    window.scroll({
+      top: position - headerHeight,
+      behavior: 'smooth',
+    })
+  }
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} id="header">
       <ul className={styles.header__navbar}>
-        {NAV_ITEMS.map((item) => (
-          <li key={item} className={styles.header__navbar__item}>
+        {Object.values(NAV_ITEMS).map((item) => (
+          <li key={item} className={styles.header__navbar__item} onClick={() => handleJumpToAnchor(item)}>
             {item}
           </li>
         ))}
