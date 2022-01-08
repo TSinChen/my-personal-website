671a3ab31d0fc4547c400033f3044801e9e115d2
TSin Chen <sinin60076@gmail.com>
2022-01-08 22:49:45 +0800

    handle button disabled

--- src/components/Utils/Button/Button.tsx
+++ src/components/Utils/Button/Button.tsx
@@ -1,15 +1,21 @@
 import { ButtonHTMLAttributes } from 'react'
+import cx from 'classnames'
 
 import styles from './style.module.scss'
 
 type Props = {
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type']
   children: string
+  disabled?: ButtonHTMLAttributes<HTMLButtonElement>['disabled']
 }
 
-const Button = ({ type = 'button', children }: Props) => {
+const Button = ({ type = 'button', children, disabled }: Props) => {
   return (
-    <button className={styles.button} type={type}>
+    <button
+      className={cx(styles.button, disabled ? styles['button--disabled'] : {})}
+      type={type}
+      disabled={disabled}
+    >
       {children}
     </button>
   )
