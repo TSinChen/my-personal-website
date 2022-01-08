9d7b562ea7e3eeef8d7f228f97aed75337a211ca
TSin Chen <sinin60076@gmail.com>
2022-01-08 15:01:20 +0800

    repository type

--- src/type/personalInfo.ts
+++ src/type/personalInfo.ts
@@ -7,3 +7,23 @@ export type Experience = {
 }
 
 export type ExperienceList = Experience[]
+
+export type Repository = {
+  id: number
+  name: string
+  description: string
+  // eslint-disable-next-line camelcase
+  html_url: string
+  homepage: string
+  // eslint-disable-next-line camelcase
+  created_at: string
+  // eslint-disable-next-line camelcase
+  updated_at: string
+}
+
+export type RepositoryList = Repository[]
+
+export type GithubUser = {
+  // eslint-disable-next-line camelcase
+  html_url: string
+}
